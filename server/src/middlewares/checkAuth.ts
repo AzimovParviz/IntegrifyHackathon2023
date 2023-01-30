0d022c7e6f8190e2fd40a90fd19af0cf1abc6232
Parviz Azimov <parviz.azimov@student.lamk.fi>
2023-01-30 17:13:27 +0200

    folder structure and placeholder files

---
+++ server/src/middlewares/checkAuth.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+import { ForbiddenError } from '../helpers/apiError'
+import { JWT_SECRET } from '../util/secrets'
+
+export default function (req: Request, res: Response, next: NextFunction) {
+  try {
+    const authorizationHeader = req.headers.authorization
+    if (authorizationHeader) {
+      console.log('authorizationHeader:', authorizationHeader)
+      const token = authorizationHeader.split(' ')[1]
+
+      const decodedUser = jwt.verify(token, JWT_SECRET)
+      console.log('decodedUser:', decodedUser)
+
+      req.user = decodedUser
+      return next()
+    }
+    throw new ForbiddenError()
+  } catch (error) {
+    throw new ForbiddenError()
+  }
+}
+
