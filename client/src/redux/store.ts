e4a74500d53e0335ed5881214ae135966bbcab54
Quan Nguyen <quan.nguyen.suomea@gmail.com>
2023-01-31 11:29:43 +0200

    temporary commit

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,4 +1,6 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from '../redux/store';
 import { projectReducer } from "./reducers/projectReducer";
 import { userReducer } from "./reducers/userReducer";
 import { categoryReducer } from "./reducers/categoryReducer";
@@ -17,3 +19,7 @@ export const store = configureStore({
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
